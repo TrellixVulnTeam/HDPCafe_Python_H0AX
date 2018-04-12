916a3b90d7d71dbb3584b124b979ff8397c3aab1
Kucing Item Putih <poncoe4mac@gmail.com>
2018-04-12 11:11:25 +0700

    improv auth

--- core/auth.py
+++ core/auth.py
@@ -3,16 +3,34 @@
 # untuk ini buat jalaninnya harus pake CMD gabisa pake pycharm, soalnya di pycharm masih ada bugnya masalah getPass!
 
 import getpass
+import os
+import header
 
 def auth():
-    usernameAdmin = input("Masukan Username = ")
-    passwordAdmin = getpass.getpass("Masukan Password")
+    print("=============================")
+    print("Selamat Datang di HDP Mart!")
+    print("=============================")
+    print()
 
-    if (usernameAdmin == 'admin' and passwordAdmin == 'admin'):
-        print("Selamat Datang!")
-    else:
+    z = 'ya'
+    a = 0
+    while z == 'ya' :
+        user = input('Username = ')
+        pwd = getpass.getpass('Password = ')
+        if (user =='admin') and (pwd =='admin') :
+            print ('Selamat Datang',user,'Tekan Enter untuk lanjut')
+            input()
+            break
+        elif (user =='admin') or (pwd =='mimin') :
+            print ('Username/Password salah')
+        else :
+            print('Password Salah')
+        a = a+1
+        if a == 3 :
+            print ('sudah 3x input')
+            break
         print()
-        print("Username atau Password Salah!")
+        z = input('Input username dan password lagi ? ya/tidak ?')
         print()
-
-    return auth()
+        os.system('cls')
+        header.header()
