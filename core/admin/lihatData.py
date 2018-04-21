4596253507fb0d7f90b700a854172ceaf8c1b1ac
mystald <hillboy.peacer@gmail.com>
2018-04-21 11:14:58 +0700

    deleted unused module/file

--- core/admin/lihatData.py
+++
@@ -1,7 +0,0 @@
-# menampilkan data dari parameter b
-
-def lihatData(b):
-    print("Kode Menu\tNama Menu\tHarga Menu")
-
-    for x in range(len(b)):
-        print('%s\t%s\t%s' % (b[x][0], b[x][1], b[x][2]))
