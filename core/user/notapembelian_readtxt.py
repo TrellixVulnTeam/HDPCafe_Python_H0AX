9a15cd697d40ad0b5ed1d2e5261b79401c670e84
Kucing Item Putih <poncoe4mac@gmail.com>
2018-04-21 09:12:01 +0700

    added read nota pembelian

--- core/user/notapembelian_readtxt.py
+++ core/user/notapembelian_readtxt.py
@@ -5,7 +5,7 @@ import os
 
 def notapembelian():
     scriptpath = os.path.dirname(__file__)
-    filename = os.path.join(scriptpath, 'user.txt')
+    filename = os.path.join(scriptpath, 'notapembelian.txt')
     conn=open(filename, "r")
     acc=json.load(conn)
     conn.close()
